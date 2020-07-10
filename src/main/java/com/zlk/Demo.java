620bdedbe9df67f2433a63eb0389f366ba5e1f24
wzc <wangzhoupang@outlook.com>
2020-07-10 14:09:07 +0800

    测试提交

---
+++ src/main/java/com/zlk/Demo.java
@@ -0,0 +1,12 @@
+package com.zlk;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("测试提交");
+        System.out.println("测试提交");
+        System.out.println("测试提交");
+        System.out.println("测试提交");
+        System.out.println("测试提交");
+
+    }
+}
